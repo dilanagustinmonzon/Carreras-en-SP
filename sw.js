b4fe5b7cba1372647fa7fe363cd5f800e4ca7543
dilanagustinmonzon <dilanagustinmonzon2025@gmail.com>
2026-07-26 15:08:43 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    pueden seguir viendo la versión anterior por un tiempo.
    ===================================================================== */
 
-const CACHE_NAME = "ov-cache-v7";
+const CACHE_NAME = "ov-cache-v8";
 
 // Archivos propios del sitio que se guardan apenas se instala el Service Worker.
 const PRECACHE_URLS = [
